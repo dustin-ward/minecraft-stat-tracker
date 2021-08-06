97583fcd6643035607abc9073c3fbd5634c096c8
dustin-ward <wardddustin@gmail.com>
2021-08-05 20:50:30 -0600

    Adding Windows Support

--- data/db.go
+++ data/db.go
@@ -1,26 +1,26 @@
-package data
+// package data
 
-import (
-	"database/sql"
+// import (
+// 	"database/sql"
 
-	"github.com/dustin-ward/minecraft-time-logging/util"
-	_ "github.com/mattn/go-sqlite3"
-)
+// 	"github.com/dustin-ward/minecraft-time-logging/util"
+// 	_ "github.com/mattn/go-sqlite3"
+// )
 
-func Setup() *sql.DB {
-	db, err := sql.Open("sqlite3", "./database.db")
-	util.ErrorCheck(err)
-	defer db.Close()
+// func Setup() *sql.DB {
+// 	db, err := sql.Open("sqlite3", "./database.db")
+// 	util.ErrorCheck(err)
+// 	defer db.Close()
 
-	statement, err := db.Prepare(
-		`CREATE TABLE IF NOT EXISTS users (
-			id INTEGER PRIMARY KEY,
-			username TEXT,
-			total_time FLOAT
-		)`,
-	)
-	util.ErrorCheck(err)
-	statement.Exec()
+// 	statement, err := db.Prepare(
+// 		`CREATE TABLE IF NOT EXISTS users (
+// 			id INTEGER PRIMARY KEY,
+// 			username TEXT,
+// 			total_time FLOAT
+// 		)`,
+// 	)
+// 	util.ErrorCheck(err)
+// 	statement.Exec()
 
-	return db
-}
+// 	return db
+// }
